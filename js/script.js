1d07499179d061778afb07580b2d975726d45245
nicomanunta <nic.manun@gmail.com>
2023-12-04 15:15:50 +0100

    MILESTONE 0

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// MILESTONE 0 array di oggetti
+let dipendenti = [
+    {
+        nome: "Wayne Bamett",
+        ruolo: "Founder & CEO",
+        foto: "wayne-bamett-founder-ceo.jpg angela-caroll-chief-editor.jpg"
+    },
+    {
+        nome: "Angela Caroll",
+        ruolo: "Chief Editor",
+        foto: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        nome: "Walter Gordon",
+        ruolo: "Office manager",
+        foto: "walter-gordon-office-manager.jpg",
+    },
+    {
+        nome: "Angela Lopez",
+        ruolo: "Social media manager",
+        foto: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        nome: "Scott Estrada",
+        ruolo: "Developer",
+        foto: "scott-estrada-developer.jpg",
+    },
+    {
+        nome: "Barbara Ramos",
+        ruolo: "Graphic Designer",
+        foto: "barbara-ramos-graphic-designer.jpg",
+    }
+]
+console.log(dipendenti)
+
+
+
+
