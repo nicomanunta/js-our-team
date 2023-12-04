3aba7cf94122be6266d5f3ce9ac7fb36124e5d9b
nicomanunta <nic.manun@gmail.com>
2023-12-04 17:02:41 +0100

    Corretta milestone 2 e bonus 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // MILESTONE 0 array di oggetti
 let dipendenti = [
     {
-        nome: "Wayne Bamett",
+        nome: "Wayne Barnett",
         ruolo: "Founder & CEO",
-        foto: "wayne-bamett-founder-ceo.jpg"
+        foto: "wayne-barnett-founder-ceo.jpg"
     },
     {
         nome: "Angela Caroll",
@@ -35,21 +35,20 @@ console.log(dipendenti)
 // MILESTONE 1 stampare in console
 for(let i = 0; i<dipendenti.length; i++){
     
-    for (let key in dipendenti[i]){
-        
-        console.log(dipendenti[i].nome)
-        console.log(dipendenti[i].ruolo)
-        console.log(dipendenti[i].nome)
-    }
+    console.log(dipendenti[i].nome)
+    console.log(dipendenti[i].ruolo)
+    console.log(dipendenti[i].foto)
 }
+let dipendente = document.getElementById("dipendente")
 // MILESTONE 2 STAMPERE NEL DOM
-dipendenti[0].foto.search = "wayne-bamett-founder-ceo.jpg"
-document.getElementById("dipendente1").innerHTML = dipendenti[0].nome +" "+ dipendenti[0].ruolo +" "+ `<img src="./img/wayne-barnett-founder-ceo.jpg" alt="">`
-document.getElementById("dipendente2").innerHTML = dipendenti[1].nome +" "+ dipendenti[1].ruolo +" "+ `<img src="./img/angela-caroll-chief-editor.jpg" alt="">` 
-document.getElementById("dipendente3").innerHTML = dipendenti[2].nome +" "+ dipendenti[2].ruolo +" "+ `<img src="./img/walter-gordon-office-manager.jpg" alt="">` 
-document.getElementById("dipendente4").innerHTML = dipendenti[3].nome +" "+ dipendenti[3].ruolo +" "+ `<img src="./img/angela-lopez-social-media-manager.jpg" alt="">` 
-document.getElementById("dipendente5").innerHTML = dipendenti[4].nome +" "+ dipendenti[4].ruolo +" "+ `<img src="./img/scott-estrada-developer.jpg" alt="">`
-document.getElementById("dipendente6").innerHTML = dipendenti[5].nome +" "+ dipendenti[5].ruolo +" "+ `<img src="./img/barbara-ramos-graphic-designer.jpg" alt="">`
+ for(let i = 0; i<dipendenti.length; i++){
+    let div = document.createElement("div")
+    div.innerHTML = dipendenti[i].nome +" "+ dipendenti[i].ruolo +" "+ dipendenti[i].foto +" "+ `<img class="dimensioni-img" src="./img/${dipendenti[i].foto}" alt="">`
+    dipendente.appendChild(div)
+    
+    
+ }
+
 
 
 
